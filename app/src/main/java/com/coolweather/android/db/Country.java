e6c1ad73a5f31f42df3a47bd93706ed77d2d86e8
laojunwei <675880289@qq.com>
2017-09-15 10:33:43 +0800

    加入创建数据库和表格的各项配置。

---
+++ app/src/main/java/com/coolweather/android/db/Country.java
@@ -0,0 +1,45 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+/**
+ * Created by Administrator on 2017/9/15.
+ */
+
+public class Country extends DataSupport{
+    private int id;
+    private String countryName;
+    private String weatherID;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherID() {
+        return weatherID;
+    }
+
+    public void setWeatherID(String weatherID) {
+        this.weatherID = weatherID;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
